99747a130664514fd8ebef0fff0e9566402087c5
Sephora <seph156@gmail.com>
2018-05-08 15:35:00 -0700

    intro practice problems

---
+++ Intro_to_http/practice_problems.rb
@@ -0,0 +1,7 @@
+# 1. required: Host. Path, HTTP Method.
+#    optional: parameters, other headers, message body
+
+# 2. required: status.   optional: headers, body
+
+# 3. If you need to update some piece of data on the server, use POST.
+#    If you need to request some data from the server, use GET.
