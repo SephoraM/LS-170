3bfc710272d741824ff147dbc1b4952ac9e9c18d
Sephora <seph156@gmail.com>
2018-05-29 14:12:22 -0700

    lesson practice questions

---
+++ Securing_apps/practice_problems.rb
@@ -0,0 +1,7 @@
+# 1. The parameters sent via the body with a POST HTTP method are not safer than
+#    the parameters sent via the URL with a GET HTTP request. A malicious hacker
+#    who understood how HTTP works would be just as capable of accessing either
+#    unless other means of protection are used.
+
+# 2. By using HTTPS because the data is encypted and attackers wont be able to
+#    understand the data that is being transmitted.
